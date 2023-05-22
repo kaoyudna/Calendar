a6b64b54c1280ba59270eb6e4ab864c5839d0bc5
kaoyudna <121837465+kaoyudna@users.noreply.github.com>
2023-05-22 08:45:19 +0000

    [Add]

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,23 +1,27 @@
 class SchedulesController < ApplicationController
+  before_action :authenticate_user!
 
   def month
-    @schedules = Schedule.all
+    @schedules = Schedule.where(user_id: current_user.id)
   end
 
   def week
-    @schedules = Schedule.all
+    @schedules = Schedule.where(user_id: current_user.id)
   end
 
   def new
     @schedule = Schedule.new
     @default_date = params[:default_date].to_date
+    # 遷移元のセッションを保存
+    session[:previous_url] = request.referer
   end
 
   def create
     @schedule = Schedule.new(schedule_params)
     @schedule.user_id = current_user.id
-    if @schedule.save!
-      redirect_to month_schedules_path
+    if @schedule.save
+      # newの1つ前のページへ遷移
+      redirect_to session[:previous_url]
     else
       render :new
     end
@@ -25,18 +29,25 @@ class SchedulesController < ApplicationController
 
   def edit
     @schedule = Schedule.find(params[:id])
+    # 遷移元のセッションを保存
+    session[:previous_url] = request.referer
   end
 
   def update
     @schedule = Schedule.find(params[:id])
     if @schedule.update(schedule_params)
-      redirect_to month_schedules_path
+      # editの1つ前のページへ遷移
+      redirect_to session[:previous_url]
     else
       render :new
     end
   end
 
   def destroy
+    @schedule = Schedule.find(params[:id])
+    @schedule.destroy
+    # editの1つ前のページへ遷移
+    redirect_to session[:previous_url]
   end
 
 
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,3 +1,5 @@
 class Schedule < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true
 end
